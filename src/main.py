bb347d5bebbbeac2c6f698dab38b8ab1a46a8158
sawada <use.github.mail@gmail.com>
2025-12-30 16:49:47 +0900

    ver. 0.2.0

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 """main process."""
 
+import os
+import time
 import tkinter as tk
+from concurrent.futures import ProcessPoolExecutor
 from pathlib import Path
 from tkinter import filedialog
 
+import psutil
 from PIL import Image
 
 import my_logger
@@ -11,26 +15,52 @@ import settings
 
 logger = my_logger.my_logger(__name__)
 
-root = tk.Tk()
-root.withdraw()
 
-dir_path = filedialog.askdirectory(title="フォルダを選択してください。")
-path = Path(dir_path)
+def convert_image(file: Path) -> None:
+    """1ファイル処理(子プロセス側)"""
+    # CPU使用コア制限(例: 最初のNコアのみ)
+    p = psutil.Process(os.getpid())
+    p.cpu_affinity(list(range(settings.MAX_WORKERS)))
 
-
-patterns = settings.FILE_TYPES
-file_list = []
-for pattern in patterns:
-    for file_path in path.rglob(pattern):
-        file_list.append(file_path)  # noqa: PERF402
-
-
-for file in file_list:
-    dst = file.with_suffix(".webp")
     try:
         with Image.open(file) as img:
+            start = time.time()
+
+            dst = file.with_suffix(".webp")
             img.save(dst, quality=settings.QUALITY)
+
+            # 擬似CPU制限
+            elapsed = time.time() - start
+            sleep_time = elapsed * (1 - settings.CPU_LOAD) / settings.CPU_LOAD
+            if sleep_time > 0:
+                time.sleep(sleep_time)
+
     except Exception as e:
         logger.exception(e)  # noqa: TRY401
 
-my_logger.shutdown()
+
+def main() -> None:  # noqa: D103
+    root = tk.Tk()
+    root.withdraw()
+
+    dir_path = filedialog.askdirectory(title="フォルダを選択してください")
+    if not dir_path:
+        return
+
+    path = Path(dir_path)
+    patterns = settings.FILE_TYPES
+
+    files = []
+    for ptn in patterns:
+        files.extend(path.rglob(ptn))
+
+    if not files:
+        return
+
+    with ProcessPoolExecutor(max_workers=settings.MAX_WORKERS) as executor:
+        executor.map(convert_image, files)
+
+
+if __name__ == "__main__":
+    main()
+    my_logger.shutdown()
